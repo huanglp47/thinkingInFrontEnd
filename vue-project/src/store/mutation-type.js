976d277278c3ef130bff765b51a03c7f52270b67
huanglipeng1 <huanglipeng/Galaxy@2019>
2020-04-15 15:04:07 +0800

    删除

--- vue-project/src/store/mutation-type.js
+++
@@ -1,9 +0,0 @@
-export const LOGIN = 'LOGIN';
-
-export const GETCONFIG = 'GETCONFIG';
-
-export const SET_TOKEN = 'SET_TOKEN'//设置全局token
-
-export const UPDATE_LOADING_STATUS = 'UPDATE_LOADING_STATUS'//页面加载
-
-export const SET_USER_INFO = 'SET_USER_INFO'//设置用户信息
